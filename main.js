89053501d597293593fd6dfb9dd5c2472e39fd99
igor-0 <106090301+igor-0@users.noreply.github.com>
2024-08-23 12:24:38 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
-    console.log
-}
+    botoes[i].onclick = function(){
+      
+        for(let j=0;j<botoes.length;j++){
+
+        
+        }
+        botoes[i].classList.add("ativo");
+    
+    console.log()
+
